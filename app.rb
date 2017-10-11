270cfe4d70ff298ebf2d2ce2d40d503f9be280c1
ainsleybc <ab.chang@me.com>
2017-10-11 15:54:51 +0100

    added new route for entering player names

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ class Battle < Sinatra::Base
 
 get '/' do
   "Testing infrastructure working!"
+  erb :index
+end
+
+post '/names' do
+  # player1 = params[:player_1]
+  
+  erb :play, { locals: { player_1: params[:player_1],player_2: params[:player_2] } }
 end
 
   run! if app_file == $0
--- spec/features/features_test_spec.rb
+++ spec/features/features_test_spec.rb
@@ -1,9 +1,22 @@
+require "capybara/rspec"
 require_relative '../../app.rb'
 
-feature "display testing infrastructure" do
-  scenario "go to homepage" do
-    visit("/")
+Capybara.app = Battle
 
+feature "display testing infrastructure" do
+  xscenario "go to homepage" do
+    visit("/")
     expect(page).to have_content "Testing infrastructure working!"
   end 
+end
+
+feature "starting a fight" do
+  scenario "enter names on the home page, then see them on screen" do
+    visit("/")
+    # require 'launchy'; save_and_open_page
+    fill_in :player_1, with: "Ainsley"
+    fill_in :player_2, with: "Canace"
+    click_button "submit"
+    expect(page).to have_content("Ainsley","Canace")
+  end
 end
