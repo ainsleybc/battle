de98a2243dd41c378bfcc5c9f6ebf14a5e61a750
Canace Wong <canacewongtoday@hotmail.com>
2017-10-11 16:39:38 +0100

    redirect play route

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
 get '/' do
   "Testing infrastructure working!"
@@ -9,8 +10,13 @@ end
 
 post '/names' do
   # player1 = params[:player_1]
-  
-  erb :play, { locals: { player_1: params[:player_1],player_2: params[:player_2] } }
+  session[:player_1_name] = params[:player_1_name]
+  session[:player_2_name] = params[:player_2_name]
+  redirect to '/play'
+end
+
+get '/play' do
+  erb :play, { locals: { player_1: session[:player_1_name], player_2: session[:player_2_name] } }
 end
 
   run! if app_file == $0
--- spec/features/features_test_spec.rb
+++ spec/features/features_test_spec.rb
@@ -7,15 +7,15 @@ feature "display testing infrastructure" do
   xscenario "go to homepage" do
     visit("/")
     expect(page).to have_content "Testing infrastructure working!"
-  end 
+  end
 end
 
 feature "starting a fight" do
   scenario "enter names on the home page, then see them on screen" do
     visit("/")
     # require 'launchy'; save_and_open_page
-    fill_in :player_1, with: "Ainsley"
-    fill_in :player_2, with: "Canace"
+    fill_in :player_1_name, with: "Ainsley"
+    fill_in :player_2_name, with: "Canace"
     click_button "submit"
     expect(page).to have_content("Ainsley","Canace")
   end
