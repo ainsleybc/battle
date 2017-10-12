a068db61d1bdd888328f8b3e2aa98d07d3513aea
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-10-12 12:17:40 +0100

    change to attack erb

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ get '/play' do
 end
 
 get '/attack' do
-  
-  $player_1.attack($player_2)
+  Game.new.attack($player_2)
   erb :attack, { locals: { player_1_name: $player_1.name, player_2_name: $player_2.name }}
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,10 @@
 require 'game'
-require 'player'
 
 describe Game do
 
   subject(:game)  { described_class.new }
-  let(:player2)   { Player.new('Lucy')}
+  let(:player1)   { double :player1 }
+  let(:player2)   { double :player2 }
 
   it 'attacks a player' do
     expect(player2).to receive(:take_hit)
