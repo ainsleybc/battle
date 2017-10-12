2318635bd05b077b1faaf3c2dd2eb945ce789265
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-10-12 10:24:11 +0100

    using global variables and creating a player class

--- app.rb
+++ app.rb
@@ -1,26 +1,26 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
 
 get '/' do
-  "Testing infrastructure working!"
   erb :index
 end
 
 post '/names' do
-  session[:player_1_name] = params[:player_1_name]
-  session[:player_2_name] = params[:player_2_name]
+  $player_1 = Player.new(params[:player_1_name])
+  $player_2 = Player.new(params[:player_2_name])
   redirect to '/play'
 end
 
 get '/play' do
-  erb :play, { locals: { player_1: session[:player_1_name], player_2: session[:player_2_name],
+  erb :play, { locals: { player_1_name: $player_1.name, player_2_name: $player_2.name,
     player_1_hp: 100,   player_2_hp: 100 } }
 end
 
 get '/attacked' do
-  erb :attacked, { locals: { player_1: session[:player_1_name], player_2: session[:player_2_name] }}
+  erb :attacked, { locals: { player_1_name: $player_1.name, player_2_name: $player_2.name }}
 end
 
   run! if app_file == $0
